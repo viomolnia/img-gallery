d0d3a7f6f5160b1985fe222a1fd621fea40d1750
Anna Hrunova <anna.hrunova@4finance.com>
2017-06-02 16:27:35 +0300

    formatting

--- index.php
+++ index.php
@@ -1,28 +1,28 @@
 <html>
-<body>
-<table width="744" border="1">
-    <tr>
-        <th height="41" scope="row">
-            <a href="index.php?option=upload" style="margin-left:100px">Upload image</a>
-            <a href="index.php?option=gallery" style="margin-left:100px">Image gallery</a>
-        </th>
-    </tr>
-    <tr>
-        <th height="401" scope="row">
-            <?php
-            @$gall = $_GET['option'];
-            switch ($gall) {
-                case 'upload':
-                    include('upload.php');
-                    break;
-                case 'gallery':
-                    include('gallery.php');
-                    break;
-            }
-            ?>
-        </th>
-    </tr>
-</table>
-</body>
+    <body>
+        <table width="744" border="1">
+            <tr>
+                <th height="41" scope="row">
+                    <a href="index.php?option=upload" style="margin-left:100px">Upload image</a>
+                    <a href="index.php?option=gallery" style="margin-left:100px">Image gallery</a>
+                </th>
+            </tr>
+            <tr>
+                <th height="401" scope="row">
+                    <?php
+                    @$gall = $_GET['option'];
+                    switch ($gall) {
+                        case 'upload':
+                            include('upload.php');
+                            break;
+                        case 'gallery':
+                            include('gallery.php');
+                            break;
+                    }
+                    ?>
+                </th>
+            </tr>
+        </table>
+    </body>
 </html>
 
